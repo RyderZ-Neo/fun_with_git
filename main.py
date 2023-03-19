fb4652710bd9bd416084d9d641035a35425d9936
ANA <ayenyeinaung666@gmail.com>
2023-03-19 14:30:51 +0800

    tested basic OOP with python

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-x = 1
-y = x*2
-z =[x for x in range(0,y+1)]
-g = z
-print(z)
-print(x,y)
+class Animal:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def speak(self):
+        print("i am an animal")
+class Dog(Animal):
+    def speak(self):
+        print("i am a dog named", self.name)
+        print("i am "+ str(self.age)+ "years old." )
+'''animal = Animal(name='fluu',age= 5)
+animal.speak()'''
+
+dog1 = Dog(name='william',age=7)
+dog1.speak()
